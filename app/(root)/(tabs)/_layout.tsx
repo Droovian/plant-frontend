50af830bcaaf0de564e7f6d3b1a5cb6006ce05ca
Shivam Raikar <90413359+Proshivam23@users.noreply.github.com>
2025-06-19 10:23:28 +0530

    Add Alerts tab and implement AlertsComponent for user notifications

--- app/(root)/(tabs)/_layout.tsx
+++ app/(root)/(tabs)/_layout.tsx
@@ -59,6 +59,14 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="person.fill" color={color} />,
         }}
       />
+      <Tabs.Screen
+        name="alerts"
+        options={{
+          title: 'Alerts',
+          headerShown: false,
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="person.fill" color={color} />,
+        }}
+      />
     </Tabs>
   );
 }
